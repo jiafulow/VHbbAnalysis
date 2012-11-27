26d214869613088a59ae9b01c9a43484d85713a9
jiafulow <>
2012-11-27 16:00:03 +0000

    - Beautified by `indent -i4 -br -ce -brf -nut -nprs -npcs -npsl`. - Got rid of C++ "reorder" warnings. - Added in PUInfo: intimePU. - Changed in PUInfo: truePU is now a float.

--- VHbbDataFormats/interface/VHbbEventAuxInfo.h
+++ VHbbDataFormats/interface/VHbbEventAuxInfo.h
@@ -1,72 +1,73 @@
-#ifndef VHbbEventAuxInfo__H 
-#define VHbbEventAuxInfo__H 
+#ifndef VHbbEventAuxInfo__H
+#define VHbbEventAuxInfo__H
 
-#include <TLorentzVector.h>
-#include <TVector2.h>
-#include "Math/SMatrix.h"
 #include <vector>
-#include<map>
-class VHbbEventAuxInfo{
- public:
+#include <map>
+
+#include "TLorentzVector.h"
+#include "TVector2.h"
+#include "Math/SMatrix.h"
 
 
-  class PrimaryVertexInfo {
+class VHbbEventAuxInfo {
   public:
-    PrimaryVertexInfo() : nVertices (-99){}
-    TVector3 firstPVInPT2,firstPVInProb;
-    ROOT::Math::SMatrix<double, 3, 3, ROOT::Math::MatRepSym<double, 3> > efirstPVInPT2,efirstPVInProb;
-    int nVertices;
-  };
+    class PrimaryVertexInfo {
+      public:
+        PrimaryVertexInfo(): nVertices(-99) {}
+        int nVertices;
+        TVector3 firstPVInPT2, firstPVInProb;
+        ROOT::Math::SMatrix < double, 3, 3, ROOT::Math::MatRepSym < double, 3 > > efirstPVInPT2, efirstPVInProb;
+    };
 
-  class PUInfo{
-  public:
-    PUInfo(): rho(-99), rho25(-99),rho25Iso(-99),rhoNeutral(-99),truePU(9999) {}
-  public:
-    float rho, rho25,rhoNeutral,rho25Iso;
-    std::map<int,unsigned int>  pus;
-    unsigned int truePU;
-  };
+    class PUInfo {
+      public:
+        PUInfo(): rho(-99), rho25(-99), rho25Iso(-99), rhoNeutral(-99), intimePU(99), truePU(9999), pus() {}
+      public:
+        float rho, rho25, rho25Iso, rhoNeutral;
+        int intimePU;
+        float truePU;
+        std::map < int, unsigned int > pus;
+    };
 
+    class ParticleMCInfo {
+      public:
+        ParticleMCInfo(): status(-99), momid(-99), gmomid(-99), charge(-99), dauid(), dauFourMomentum() {}
+      public:
+        int status;
+        int momid;
+        int gmomid;
+        float charge;
+        TLorentzVector p4;
+        //int ndau;
+        std::vector < int > dauid;
+        std::vector < TLorentzVector > dauFourMomentum;
+    };
 
-  class ParticleMCInfo {
-  public:
-    ParticleMCInfo(): status(-99), momid(-99), gmomid(-99), charge(-99){}
-  public:
-    int status;
-    int momid;
-    int gmomid;
-    float charge;
-    TLorentzVector p4;
-    //    int ndau;
-    std::vector<int> dauid;
-    std::vector<TLorentzVector> dauFourMomentum;    
-  };
+    double genBBDeltaR() const {
+        if (mcB.size() > 0 && mcBbar.size() > 0)
+            return mcB[0].p4.DeltaR(mcBbar[0].p4);
+        else
+            return -99;
+    }
+    
+    double genCCDeltaR() const {
+        if (mcC.size() >= 2)
+            return mcC[0].p4.DeltaR(mcC[1].p4);
+        else
+            return -99;
+    }
 
- double genBBDeltaR() const
- {
-   if(mcB.size() > 0 && mcBbar.size() > 0)
-   return mcB[0].p4.DeltaR(mcBbar[0].p4);
-   else return -99;
- }
- double genCCDeltaR() const
- {
-   if(mcC.size() >=2)
-   return mcC[0].p4.DeltaR(mcC[1].p4);
-   else return -99;
- }
- 
- public:
-  PUInfo puInfo;
-  float weightMCProd;  
- PrimaryVertexInfo pvInfo;
-  std::vector<ParticleMCInfo> mcH;
-  std::vector<ParticleMCInfo> mcW;
-  std::vector<ParticleMCInfo> mcZ;
-  
-  std::vector<ParticleMCInfo> mcBbar;
-  std::vector<ParticleMCInfo> mcB;
-  std::vector<ParticleMCInfo> mcC;       
+    PrimaryVertexInfo pvInfo;
+    PUInfo puInfo;
+    std::vector < ParticleMCInfo > mcH;
+    std::vector < ParticleMCInfo > mcW;
+    std::vector < ParticleMCInfo > mcZ;
+    std::vector < ParticleMCInfo > mcTau;
+    std::vector < ParticleMCInfo > mcBbar;
+    std::vector < ParticleMCInfo > mcB;
+    std::vector < ParticleMCInfo > mcC;
+    float weightMCProd;
 
-  std::vector<ParticleMCInfo> mcTau;
 };
-#endif
+#endif // VHbbEventAuxInfo__H
+
