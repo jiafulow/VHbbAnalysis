b296154e52149e4d0c224187c1a1b9fd17b9c2eb
Jia Fu Low <jia.fu.low@cern.ch>
2014-03-01 11:16:11 -0600

    Removed ZmumuHbbSkim

--- ZmumuHbbSkim/plugins/PATObjectMCMatchFilter.cc
+++
@@ -1,9 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "VHbbAnalysis/ZmumuHbbSkim/plugins/PATObjectMCMatchFilter.h"
-
-using namespace pat;
-
-DEFINE_FWK_MODULE(PATJetMCMatchFilter);
-DEFINE_FWK_MODULE(PATMuonMCMatchFilter);
-DEFINE_FWK_MODULE(PATElectronMCMatchFilter);
-
